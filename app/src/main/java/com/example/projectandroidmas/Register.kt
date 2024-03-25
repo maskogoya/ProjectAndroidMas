7b2b90037b872c3a62f596d0bb107c334b8abc0f
Mas Kogoya <maskogoya007@gmail.com>
2024-03-26 02:28:02 +0900

    Revisi 1 Eksplicit Intent

--- app/src/main/java/com/example/projectandroidmas/Register.kt
+++ app/src/main/java/com/example/projectandroidmas/Register.kt
@@ -1,11 +1,28 @@
 package com.example.projectandroidmas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.projectandroidmas.databinding.ActivityLoginBinding
+import com.example.projectandroidmas.databinding.ActivityRegisterBinding
 
 class Register : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        setContentView(binding.root)
+
+        // Tombol (button) navigasi ke halaman login
+        binding.btnRegister.setOnClickListener {
+            val intentActivityLogin = Intent(this, ActivityLoginBinding::class.java)
+            startActivity(intentActivityLogin)
+
+
+        // Link (text view) navigasi ke halaman login
+        binding.tvLinkSudahPunyaAkun.setOnClickListener {
+            val intentActivityLogin = Intent(this, ActivityLoginBinding::class.java)
+            startActivity(intentActivityLogin)
+        }
     }
 }
